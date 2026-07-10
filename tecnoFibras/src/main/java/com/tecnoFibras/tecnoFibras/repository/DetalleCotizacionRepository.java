560f060761d7db3cde484c9b1990ffa2e7831f15
Yulisa134 <mongeyulisa2@gmail.com>
2026-07-09 19:20:37 -0600

    DetalleCotizacionRepository YMJ

--- tecnoFibras/src/main/java/com/tecnoFibras/tecnoFibras/repository/DetalleCotizacionRepository.java
+++ tecnoFibras/src/main/java/com/tecnoFibras/tecnoFibras/repository/DetalleCotizacionRepository.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.tecnoFibras.tecnoFibras.repository;
 
-/**
- *
- * @author monge
- */
-public class DetalleCotizacionRepository {
+package com.tecnoFibras.tecnoFibras.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetalleCotizacionRepository extends JpaRepository<DetalleCotizacion, Integer> {
     
+    // Clave para recuperar todos los productos que pertenecen a una misma cotización
+    List<DetalleCotizacion> findByIdCotizacion(Integer idCotizacion);
 }
